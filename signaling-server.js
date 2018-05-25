bdff50c1c3030a2627bef22136b93d9a891cede3
Dan Pastusek <dan@hexial.com>
2018-05-25 11:23:36 -0700

    use simple-peer

--- signaling-server.js
+++ signaling-server.js
@@ -2,16 +2,11 @@ const http = require('http').createServer().listen(8080, '0.0.0.0')
 const io = require('socket.io').listen(http)
 
 io.on('connection', function (socket) {
-  console.log(`Client Connected: ${socket.id}`)
-
-  socket.on('message', function (message) {
-    console.log(`got message:`, message.type)
-    // broadcast sends to all connected clients except the one who sent this message
-    socket.broadcast.emit('message', message)
+  socket.on('signal', function (message) {
+    socket.broadcast.emit('signal', message)
   })
 
   socket.on('peer-connect', function () {
-    console.log('peer-connect')
     socket.broadcast.emit('peer-connect')
   })
 })
--- src/App.js
+++ src/App.js
@@ -1,152 +1,55 @@
-// adapted heavily from https://github.com/googlecodelabs/webrtc-web/blob/master/step-06/js/main.js
-
-/* WebRTC Connection Flow:
- * 1.
- */
-
 import React, { Component } from 'react'
 import './App.css'
 import io from 'socket.io-client'
+import Peer from 'simple-peer'
 
 const socket = io()
-
-socket.on('connect', () => {
-  console.log(socket.id) // 'G5p5...'
-})
-
-socket.on('connection', function () {
-  console.log('socket - connected to signaling server')
-})
-
-function logError (err) {
-  if (!err) return
-  if (typeof err === 'string') {
-    console.warn(err)
-  } else {
-    console.warn(err.toString(), err)
-  }
-}
-
-const rtcConfig = {
-  'iceServers': [{
-    'urls': 'stun:stun.l.google.com:19302'
-  }]
-}
-
-let peerConnection
-let dataChannel
 
 class App extends Component {
   state = {
     sender:    false,
     connected: false,
-    message: 'Lorem ipsum.'
+    message:   'Lorem ipsum.'
   }
 
   componentDidMount = () => {
     socket.on('peer-connect', () => {
-      this.createPeerConnection() // this connects as receiver
+      this.peer = new Peer()
+      this.setupPeerCallbacks()
     })
 
-    socket.on('message', (message) => {
-      switch (message.type) {
-        case 'offer':
-          //TODO I dont think this is ever called????
-          console.log('Signaling Message: Got offer. Sending answer to peer.')
-          peerConnection.setRemoteDescription(new RTCSessionDescription(message), function () {}, logError)
-          peerConnection.createAnswer(this.onLocalSessionCreated, logError)
-          break
-        case 'answer':
-          console.log('Signaling Message: Got answer.')
-          peerConnection.setRemoteDescription(new RTCSessionDescription(message), function () {}, logError)
-          break
-        case 'candidate':
-          console.log('Signaling Message: Got candidate.')
-          peerConnection.addIceCandidate(new RTCIceCandidate({candidate: message.candidate}))
-          break
-        default:
-          // TODO break these into socket.on(event) blocks
-          break
-      }
+    socket.on('signal', data => {
+      this.peer.signal(data)
     })
   }
 
-  onLocalSessionCreated (desc) {
-    console.log('local session created:', desc)
-    peerConnection.setLocalDescription(desc, function () {
-      console.log('sending local desc:', peerConnection.localDescription)
-      socket.emit('message', peerConnection.localDescription)
-    }, logError)
-  }
+  setupPeerCallbacks = () => {
+    this.peer.on('signal', function (data) {
+      // when we have our own ICE data, give it to the remote peer via the signaling server
+      socket.emit('signal', data)
+    })
 
-  createPeerConnection () {
-    // This line only reaches out to the STUN server, which tells us about ourself (IP, firewall config, etc)
-    peerConnection = new RTCPeerConnection(rtcConfig)
+    this.peer.on('connect', () => {
+      this.setState({connected: true})
+    })
 
-    // Once we get ICE candidate(s) from the STUN server, we can send it to the other peer, via the signaling server
-    peerConnection.onicecandidate = function (event) {
-      console.log('icecandidate event:', event)
-      if (event.candidate) {
-        socket.emit('message', {
-          type:      'candidate',
-          label:     event.candidate.sdpMLineIndex,
-          id:        event.candidate.sdpMid,
-          candidate: event.candidate.candidate
-        })
-      }
-    }
-
-    if (this.state.sender) {
-      console.log('Creating Data Channel')
-      dataChannel = peerConnection.createDataChannel('photos')
-      this.onDataChannelCreated(dataChannel)
-      console.log('Creating an offer')
-      peerConnection.createOffer(this.onLocalSessionCreated, logError)
-    } else {
-      peerConnection.ondatachannel = (event) => {
-        console.log('ondatachannel:', event.channel)
-        dataChannel = event.channel
-        this.onDataChannelCreated(dataChannel)
-      }
-    }
-  }
-
-  onDataChannelCreated = channel => {
-    console.log('Data Channel Created:', channel)
-
-    channel.onopen = () => {
-      console.log('Data Channel Opened.')
-      this.setState({
-        connected: true
-      })
-    }
-
-    channel.onclose = () => {
-      console.log('Data Channel Closed.')
-      this.setState({
-        connected: false,
-        message: 'Disconnected.'
-      })
-    }
-
-    channel.onmessage = (event) => {
-      this.setState({
-        message: event.data
-      })
-    }
+    this.peer.on('data', data => {
+      this.setState({message: data.toString()})
+    })
   }
 
   connectAsSender = () => {
-    socket.emit('peer-connect') // tell other peers to connect as a receiver
+    socket.emit('peer-connect') // tell other peer to connect as a receiver
     this.setState({
       sender: true
     }, () => {
-      this.createPeerConnection()
+      this.peer = new Peer({initiator: true})
+      this.setupPeerCallbacks()
     })
   }
 
   sendMessage = () => {
-    dataChannel.send(this.chatinput.value)
+    this.peer.send(this.chatinput.value)
   }
 
   render () {
@@ -156,7 +59,6 @@ class App extends Component {
         <button onClick={this.connectAsSender}>
           {this.state.connected ? `Connected as ${this.state.sender ? 'sender' : 'receiver'}` : 'Connect'}
         </button>
-        {/*<button onClick={sendData}>Send</button>*/}
       </header>
       <p className="App-intro">
         {this.state.connected && this.state.sender ?
